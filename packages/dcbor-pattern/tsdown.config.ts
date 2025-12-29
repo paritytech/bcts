fd704cbcb4f17a4d9334040b1045eb629e80efbd
Leonardo Custodio <leonardo@custodio.me>
2025-12-29 13:10:34 +0700

    Boilerplate code

---
+++ packages/dcbor-pattern/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  format: ["iife", "cjs", "esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  target: "es2022",
+  globalName: "bctsDcborPattern",
+});
